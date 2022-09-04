77022c6cc7e646eec395bee46a4597325bd72d22
Dotan Nahum <jondotan@gmail.com>
2022-09-04 18:57:03 +0300

    build: remove vergen to ease build time

--- backpack/src/bin/commands/root.rs
+++ backpack/src/bin/commands/root.rs
@@ -4,7 +4,6 @@ use clap::Command;
 
 pub fn command() -> Command<'static> {
     Command::new("backpack")
-        .version(env!("VERGEN_GIT_SEMVER"))
         .version(crate_version!())
         .subcommand_required(true)
         .arg_required_else_help(true)
